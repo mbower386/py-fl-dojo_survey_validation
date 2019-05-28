32c65602439398da2275f5dbc299e033d77f4418
mbower386 <mbower386@gmail.com>
2019-05-28 16:27:36 -0700

    finished

--- dojo_survey_validation/server.py
+++ dojo_survey_validation/server.py
@@ -1,23 +1,34 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-
+from mysqlconnection import connectToMySQL
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/result', methods=["POST"])
-def process_form():
-    print("*" * 50)
-    print(request.form)
-    print(f"username submitted: {request.form['username']}")
-    print(f"location submitted: {request.form['location']}")
-    print(f"language submitted: {request.form['language']}")
-    print(f"comment submitted: {request.form['comment']}")
+@app.route('/add_survey', methods=["POST"])
+def add_pet_to_db():
+    mysql = connectToMySQL("survey_db")
+    query = "INSERT INTO surveys (name, location, language, comment) VALUES (%(nm)s, %(lo)s, %(la)s, %(cm)s);"
+    data = {
+        'nm': request.form["username"],
+        'lo': request.form["location"],
+        'la': request.form["language"],
+        'cm': request.form["comment"]
+    }
+    mysql.query_db(query, data)
+    return redirect("/survey", )
 
-    return render_template("submitted.html", 
-    name=request.form["username"], location=request.form["location"], language=request.form["language"], comment=request.form["comment"])
+
+@app.route('/survey')
+def view_survey():
+    # call the function, passing in the name of our db
+    mysql = connectToMySQL('survey_db')
+    # call the query_db function, pass in the query as a string
+    surveys = mysql.query_db('SELECT * FROM survey_db.surveys;')
+    print(surveys)
+    return render_template("submitted.html", all_surveys=surveys)
 
 
 if __name__ == "__main__":
